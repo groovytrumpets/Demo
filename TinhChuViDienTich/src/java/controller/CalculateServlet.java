c38fa209b87689a35ce8a3b4961e3382cb144915
groovytrumpets <166611562+groovytrumpets@users.noreply.github.com>
2024-06-22 10:16:39 +0700

    CalculateServlet.java

--- TinhChuViDienTich/src/java/controller/CalculateServlet.java
+++ TinhChuViDienTich/src/java/controller/CalculateServlet.java
@@ -4,12 +4,25 @@
  */
 package controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.tomcat.util.descriptor.web.MultipartDef;
 
 /**
  *
